2525b4ec8af64c876ed9993f735a622824ceff29
maithiljanardan <maithiljanardan@gmail.com>
2021-07-03 20:21:47 +0530

    EASY: Problem : 657

---
+++ src/main/java/org/janardhan/mth/RobotReturnToZero.java
@@ -0,0 +1,34 @@
+package org.janardhan.mth;
+
+public class RobotReturnToZero {
+    public static void main(String[] args) {
+        System.out.println(judgeCircle("UDL"));
+    }
+
+    public static boolean judgeCircle(String moves) {
+
+        int x = 0;
+        int y = 0;
+        for (char ch : moves.toCharArray()) {
+            switch (ch) {
+                case 'L':
+                    x++;
+                    break;
+                case 'R':
+                    x--;
+                    break;
+                case 'U':
+                    y++;
+                    break;
+                case 'D':
+                    y--;
+                    break;
+            }
+        }
+        if (x == y && x == 0)
+            return true;
+        else
+            return false;
+
+    }
+}
